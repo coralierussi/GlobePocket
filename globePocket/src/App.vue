423b8ee6707a5baea711dd1eaff233d369521766
coralie <russicoralie812@gmail.com>
2025-02-28 09:05:13 +0100

    install dependance vue

--- globePocket/src/App.vue
+++ globePocket/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <!-- <router-view />-->
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+  data(){
+    return{
+      
+    }
+  }
 </script>
+
+<style scss scoped>
+
+</style>
