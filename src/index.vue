b3746fbc09e1486ac6ef7e5fe6d6556c2fb79acf
coralie <russicoralie812@gmail.com>
2025-02-28 13:41:13 +0100

    modif code

--- src/index.vue
+++ src/index.vue
@@ -1,17 +1,22 @@
 <template>
     <v-main class="all">
-        <Header></Header>
-        <Footer>
-            <Contact></Contact>
-        </Footer>
+        <template>
+            <div class="navbar d-flex">
+            <a class="link-header" href="./index.html">Accueil</a>
+            <p class="link-header">Pays</p>
+            <p class="link-header">Activités</p>
+            <p class="link-header">Transports</p>
+            <p class="link-header">Hébergements</p>
+            <p class="link-header">Locations</p>
+        </div>
+        </template>
     </v-main>
 </template>
 
 <script>
-import Contact from './components/Contact.vue';
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Contact from './components/Contact.vue';
+//import Header from './components/Header.vue';
+//import Footer from './components/Footer.vue';
+//import Contact from './components/Contact.vue';
 createApp(index).mount('#index');
     export default{
         name:'Index',
